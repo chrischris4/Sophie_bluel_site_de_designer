dbfb32cb90002e605127a6bacd4280ca61684f83
Jost Christopher <jostchristopher44@gmail.com>
2023-02-13 20:18:56 +0100

    création des bouton

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -14,12 +14,13 @@ console.log('hello');
 //     }
 // })
 
+
+const monSet = new Set();
+
 fetch('http://localhost:5678/api/works')
 .then((response) => response.json())
     .then((result) => {
         result.forEach(element => {
-            console.log(element)
-            console.log(element.title)
             var divElem = document.createElement('div');
             document.getElementsByClassName('gallery')[0].appendChild(divElem);
 
@@ -30,7 +31,46 @@ fetch('http://localhost:5678/api/works')
 
             var pElem = document.createElement('p');
             divElem.appendChild(pElem);
+            pElem.textContent = element.title;
+        })
 
-            pElem.textContent = element.title;            
+
+        // const objetsSet = new Set();
+        // Set.objetsSet.add(object)
+
+
+
+        var btn = document.createElement("BUTTON");
+        var t = document.createTextNode("Tous");     
+        btn.appendChild(t);
+        btn.setAttribute("class", "work-filter");
+        // btn.setAttribute("data-filter", "category'id'0");
+        document.body.appendChild(btn);
+
+
+        var btn = document.createElement("BUTTON");
+        var o = document.createTextNode("Object");     
+        btn.appendChild(o);
+        btn.setAttribute("class", "work-filter");
+        // btn.setAttribute("data-filter", "category'id'1");
+        document.body.appendChild(btn); 
+
+        var btn = document.createElement("BUTTON");
+        var a = document.createTextNode("Appartements");     
+        btn.appendChild(a);         
+        btn.setAttribute("class", "work-filter");
+        document.body.appendChild(btn); 
+
+        var btn = document.createElement("BUTTON");
+        var h = document.createTextNode("Hotels et restaurants");     
+        btn.appendChild(h);         
+        btn.setAttribute("class", "work-filter");
+        document.body.appendChild(btn); 
+
+        document.getElementsByClassName('work-filter')[0].addEventListener('click', function(event) {
+            console.log("clicked");
+            });
         });
-    });
+
+
+
