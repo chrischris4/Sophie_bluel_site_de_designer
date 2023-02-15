ead8d541d9d394e8905fbd34d54e08a3a7f9bd17
Jost Christopher <jostchristopher44@gmail.com>
2023-02-15 16:04:55 +0100

    button filtres update

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -15,62 +15,81 @@ console.log('hello');
 // })
 
 
-const monSet = new Set();
+const categories = [];
+categories.push({id: 0, name: 'Tous'});
 
 fetch('http://localhost:5678/api/works')
 .then((response) => response.json())
-    .then((result) => {
-        result.forEach(element => {
-            var divElem = document.createElement('div');
-            document.getElementsByClassName('gallery')[0].appendChild(divElem);
+.then((results) => {
+		console.log(results);
+    results.forEach((element, index) => {
+    		// Collecting categories
+    	categories.push(element.category);
 
-            var imgElem = document.createElement('img');
-            divElem.appendChild(imgElem);
-            imgElem.setAttribute("src", element.imageUrl);
-            imgElem.setAttribute("crossorigin", "anonymous");
+    		// Creating <figure>
+        let figureElem = document.createElement('figure');
+        figureElem.classList.add(`work-item`);
+        figureElem.classList.add(`category-id-0`);
+        figureElem.classList.add(`category-id-${element.categoryId}`);
+        document.getElementsByClassName('gallery')[0].appendChild(figureElem);
+        //document.querySelector('.gallery').appendChild(figureElem);
 
-            var pElem = document.createElement('p');
-            divElem.appendChild(pElem);
-            pElem.textContent = element.title;
-        })
+        // Creating <img>
+        let imgElem = document.createElement('img');
+        figureElem.appendChild(imgElem);
+        imgElem.setAttribute("src", element.imageUrl);
+        imgElem.setAttribute("alt", element.title);
+        imgElem.setAttribute("crossorigin", "anonymous");
 
+        // Creating <figcaption>
+        let figCaptionElement = document.createElement('figcaption');
+        figureElem.appendChild(figCaptionElement);
+        figCaptionElement.textContent = element.title;
+    });
 
-        // const objetsSet = new Set();
-        // Set.objetsSet.add(object)
+		console.log(categories);
+		let alreadyDisplayedCategoriesIds = [];
+		categories.forEach((category, index) => {
+			if(alreadyDisplayedCategoriesIds.includes(category.id) === false) {
 
+				// Adding category ID to the categories already displayed
+				alreadyDisplayedCategoriesIds.push(category.id);
 
-
-        var btn = document.createElement("BUTTON");
-        var t = document.createTextNode("Tous");     
-        btn.appendChild(t);
-        btn.setAttribute("class", "work-filter");
-        // btn.setAttribute("data-filter", "category'id'0");
-        document.body.appendChild(btn);
-
-
-        var btn = document.createElement("BUTTON");
-        var o = document.createTextNode("Object");     
-        btn.appendChild(o);
-        btn.setAttribute("class", "work-filter");
-        // btn.setAttribute("data-filter", "category'id'1");
-        document.body.appendChild(btn); 
-
-        var btn = document.createElement("BUTTON");
-        var a = document.createTextNode("Appartements");     
-        btn.appendChild(a);         
-        btn.setAttribute("class", "work-filter");
-        document.body.appendChild(btn); 
-
-        var btn = document.createElement("BUTTON");
-        var h = document.createTextNode("Hotels et restaurants");     
-        btn.appendChild(h);         
-        btn.setAttribute("class", "work-filter");
-        document.body.appendChild(btn); 
-
-        document.getElementsByClassName('work-filter')[0].addEventListener('click', function(event) {
-            console.log("clicked");
-            });
-        });
+				// Creating category filter button
+				let btn = document.createElement("button");
+				btn.textContent = category.name;
+				btn.setAttribute("class", "work-filter");
+				btn.setAttribute("data-filter", `category-id-${category.id}`);
+				document.body.appendChild(btn);
+				document.getElementsByClassName('filter')[0].appendChild(btn);
 
 
+				// Adding event listener
+				btn.addEventListener('click', function(event) {
+					console.log("clicked");
+					console.log(event.target.getAttribute('data-filter'));
+				});
 
+				// add filter for all categories
+				document.querySelectorAll('.work-filter').forEach(filterButton => {
+					filterButton.addEventListener('click', function(event) {
+						let filterValue = this.getAttribute('data-filter');
+						document.querySelectorAll('.work-item').forEach(workItem => {
+							if(!workItem.classList.contains(filterValue)) {
+								workItem.style.display = 'none';
+							} else {
+								workItem.style.display = '';
+							}
+						});
+					});
+				});
+
+				
+
+
+			}
+		});
+})
+.catch(function(err) {
+	console.log(err);
+});
--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,1 +1,42 @@
-console.log('yooo')
+document.addEventListener("DOMContentLoaded", function() {
+    const email = document.getElementsById('email', userId.json);
+    const password = document.getElementsById('mdp', token.json);
+
+    if (email.value == "")
+    {
+        alert("Mettez votre mail"); 
+        email.focus(); 
+        return false; 
+    } 
+
+    if (password.value == "")
+    {
+        alert("Mettez votre mdp"); 
+        password.focus(); 
+        return false; 
+    }
+    return true;
+});
+
+localStorage.getItem('user' , "json");
+
+const data = { username: 'userId' };
+const pwd = { passeword: 'token'};
+
+fetch('http://localhost:5678/api/users/login', {
+    method: 'POST', // or 'PUT'
+    headers: {
+    'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+})
+.then((response) => response.json())
+.then((data) => {
+    console.log('Success:', data);
+})
+.catch((error) => {
+    console.error('Error:', error);
+});;
+
+
+console.log('yooo');
